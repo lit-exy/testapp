24581b2b3fdea7f04558fbd7161754d5e633004d
Awunor <macbookair@EXY.local>
2024-05-06 20:03:35 +0100

    first commit

--- hub/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint, Flask, render_template
-
-app = Flask(__name__)
-blu = Blueprint('big', __name__)
---
+++ routes.py
@@ -0,0 +1,17 @@
+from hub import app,render_template
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
